a7c92f251ac45d99aa2e795ff7bde7eb376e1218
mandar2812 <mandar2812@gmail.com>
2013-07-18 02:26:51 +0530

    Added bootstrap view files

--- enronAnalysisWeb/graphVisualization/views.py
+++ enronAnalysisWeb/graphVisualization/views.py
@@ -4,5 +4,11 @@ from django.conf import settings
 from django.http import Http404,HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
 def index(request):
-    return HttpResponse('Hello World!!')
+    template = loader.get_template('graphVisualization/starter-template.html')
+    context = RequestContext(request, {
+        'latest_poll_list' : ['Mandar', 'Aniket', 'Subhendu']
+    })
+    return HttpResponse(template.render(context))
