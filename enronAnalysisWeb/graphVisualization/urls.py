74ef55cb9f85f0d9e50908fb190f9e13bccf1e62
Mandar Chandorkar <mandar2812@gmail.com>
2013-07-17 14:48:35 -0400

    Changes for fixing django errors in views, first steps sigh ...

--- enronAnalysisWeb/graphVisualization/urls.py
+++ enronAnalysisWeb/graphVisualization/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
+from graphVisualization.packages.views import *
 urlpatterns = patterns('graphVisualization.views',
                        (r'/admin','django.contrib.admin'),
                        (r'/(\w+)/','detail'),
-                       (r'','index'),
+                       (r'','index')
 )
--- enronAnalysisWeb/graphVisualization/views.py
+++ enronAnalysisWeb/graphVisualization/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 from django.http import Http404,HttpResponseRedirect
 from django.shortcuts import render_to_response
-
+from django.http import Httpresponse
 def index():
-    print '<html><body><h1>Hello World!!<h1></body></html>>'
+    return HttpResponse('Hello World!!')
