5b2c9623eb0dac4f3d67d40b39979430da962fc2
Mandar Chandorkar <mandar2812@gmail.com>
2013-07-17 15:28:09 -0400

    More tinkering

--- enronAnalysisWeb/graphVisualization/views.py
+++ enronAnalysisWeb/graphVisualization/views.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from django.http import Http404,HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
-def index():
+def index(request):
     return HttpResponse('Hello World!!')
