ff1031a92e2ce226cab4b4a2229a6ebe91298dc9
Mandar Chandorkar <mandar2812@gmail.com>
2013-07-17 15:20:05 -0400

    Experiment with database backend as couchDB

--- enronAnalysisWeb/enronAnalysisWeb/settings.py
+++ enronAnalysisWeb/enronAnalysisWeb/settings.py
@@ -13,21 +13,21 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django_couchdb.backends.couchdb', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'enron_email_dump',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PORT': '5984',                      # Set to empty string for default.
     }
-}'''
+}
 
-COUCHDB_DATABASES = (
+'''COUCHDB_DATABASES = (
     ('enronAnalysisWeb.graphVisualization', 'http://127.0.0.1:5984/enron_email_dump')
-    )
+    )'''
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
