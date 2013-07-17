d21bba7322703d48dec9787c781c2ae5ad552448
Mandar Chandorkar <mandar2812@gmail.com>
2013-07-17 15:15:29 -0400

    Small correction to typo of package name 'HttpResponse' in views.py file of graphVisualization app

--- enronAnalysisWeb/graphVisualization/urls.py
+++ enronAnalysisWeb/graphVisualization/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from views import *
+urlpatterns = ()
 urlpatterns = patterns('graphVisualization.views',
                        (r'/admin','django.contrib.admin'),
                        (r'/(\w+)/','detail'),
--- enronAnalysisWeb/graphVisualization/views.py
+++ enronAnalysisWeb/graphVisualization/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 from django.http import Http404,HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.http import Httpresponse
+from django.http import HttpResponse
 def index():
     return HttpResponse('Hello World!!')
